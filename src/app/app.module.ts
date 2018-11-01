8c90e674814ff2e84da122fd3ef759d0f10dd1b4
Ruslan Gonzalez <ruslanguns@gmail.com>
2018-11-01 01:55:30 +0000

    Added child Routes and child Modules for pages and shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { PagesModule } from './pages/pages.module';
+
 // routing
 import { APP_ROUTING } from './app.routes';
 
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcumbsComponent } from './shared/breadcumbs/breadcumbs.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcumbsComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    APP_ROUTING
+    APP_ROUTING,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]

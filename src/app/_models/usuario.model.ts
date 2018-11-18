4323cae0abe9b947c5f5baafaf2e76d9f218edd4
Ruslan Gonzalez <ruslanguns@gmail.com>
2018-11-18 17:02:15 +0000

    Se creo el modelo de usuarios del Mongo

---
+++ src/app/_models/usuario.model.ts
@@ -0,0 +1,23 @@
+
+/*
+Todos los usuarios tienen una serie de propiedades y métodos.
+
+Con esta clase nos permitirá utilizar todos estos datos de una
+sola vez...
+Este es un modelo de datos que nos permitirá trabajar de una
+manera ordenada sencilla, despues de cada solicitud GET, POST, PUT, etc..
+*/
+
+export class Usuario {
+
+  constructor(
+    public nombre: string,
+    public email: string,
+    public password: string,
+    public img?: string,
+    public role?: string,
+    public google?: boolean,
+    public _id?: string
+  ) {}
+
+}
